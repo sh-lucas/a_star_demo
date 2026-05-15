8951b80bb3ad340d42024b26756e3fb34bec11b4
sh-lucas <lucas.schwalm.silva@gmail.com>
2026-05-15 16:12:58 -0300

    chore: http and https doesn't work together

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 // api.js — REST + WebSocket client para o total-map-backend
 // const API_BASE = 'http://localhost:3000';
-// const API_BASE = 'https://api.totem.total.roxcode.io';
-const API_BASE = 'http://200.248.202.114:2800/api';
+const API_BASE = 'https://api.totem.total.roxcode.io';
+// const API_BASE = 'http://200.248.202.114:2800/api';
 
 export let AUTH_TOKEN = '';
 
